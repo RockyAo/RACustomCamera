a49554019c32413c63e058315f6a948c5542ad5b
Rocky <542622608@qq.com>
2015-09-21 16:07:54 +0800

    init

---
+++ RACustomCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RACustomCamera
+//
+//  Created by Rocky on 15/9/21.
+//  Copyright © 2015年 Rocky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RACustomCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RACustomCamera
+//
+//  Created by Rocky on 15/9/21.
+//  Copyright © 2015年 Rocky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
